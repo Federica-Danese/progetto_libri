10ff0e965aaac736f9984117821034feabedc734
LAPTOP-0Q0S33QL\fede1 <federica.danese98@gmail.com>
2024-06-29 11:48:33 +0200

    Add style.css, update search bar, add background and favicon, display results in cards

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('button').addEventListener('click', search);
+    document.querySelector('.search-input').addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            search();
+        }
+    });
+});
 
 async function search() {
     const query = document.querySelector('.search-input').value;
@@ -5,11 +13,16 @@ async function search() {
         try {
             const response = await axios.get('https://openlibrary.org/subjects/' + query + '.json');
             console.log(response.data.works);
+
+            const list = document.getElementById('list');
+            list.innerHTML = '';
+
             for (let work of response.data.works) {
                 const node = document.createElement("p");
+                node.classList.add('result-item');
                 const textnode = document.createTextNode(work.title);
                 node.appendChild(textnode);
-                document.getElementById("list").appendChild(node);
+                list.appendChild(node);                  
             }
         } catch (error) {
             console.error(error);
